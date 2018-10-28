b368198a262bcf75d43719ac84e1d93107c8d0f8
Nguyen Phuong Huy <phuonghuynguyen@hotmail.com>
2018-10-27 21:22:05 -0400

    add moving platforms

---
+++ Smashing Baller/Assets/Script/PlatformMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+
+    public Transform destination;
+    private Vector3 startingPostion;
+
+    [Tooltip("if the platforms moves back and forth or not")]
+    public bool repeatable = false;
+    public float speed = 1.0f;
+    public float duration = 3.0f;
+
+    float startTime, totalDistance;
+
+    // Use this for initialization
+    IEnumerator Start() {
+        startTime = Time.time;
+        startingPostion = transform.position;
+        totalDistance = Vector3.Distance(startingPostion, destination.position);
+
+        while (repeatable) {
+
+            yield return RepeatLerp(startingPostion, destination.position, duration);
+            yield return RepeatLerp(destination.position, startingPostion, duration);
+        }
+
+
+
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+
+        if (!repeatable) {
+
+            float currentDuration = (Time.time - startTime) * speed;
+
+            float journeyFraction = currentDuration / totalDistance;
+
+            this.transform.position = Vector3.Lerp(transform.position, destination.position, journeyFraction);
+        }
+
+
+
+
+    }
+    public IEnumerator RepeatLerp(Vector3 a, Vector3 b, float time) {
+
+        float i = 0.0f;
+        float rate = (1.0f / time) * speed;
+        while (i < 1.0f) {
+            i += Time.deltaTime * rate;
+            transform.position = Vector3.Lerp(a, b, i);
+            yield return null;
+        }
+    }
+
+
+}
