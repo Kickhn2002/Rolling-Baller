d4aa91efdb9a279aa20831ccde10f80c5477c06a
Nguyen Phuong Huy <phuonghuynguyen@hotmail.com>
2018-10-29 21:37:58 -0400

    add feature to make end of level coin to spin faster on trigger

---
+++ Smashing Baller/Assets/Script/ExtraSpeed.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraSpeed : MonoBehaviour {
+
+    public float extraSpeed = 50;
+    private float boostedSpeed;
+
+    // Use this for initialization
+    void Start() {
+        boostedSpeed = this.transform.parent.GetComponent<Rotate>().rotationSpeed + extraSpeed;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+
+    private void OnTriggerEnter(Collider collider) {
+        // if the speed of rotation is not the accelerated speed
+
+
+        if (this.transform.parent.GetComponent<Rotate>().rotationSpeed != boostedSpeed) {
+            this.transform.parent.GetComponent<Rotate>().rotationSpeed = boostedSpeed;
+
+        }
+
+
+    }
+}
---
+++ Smashing Baller/Assets/disapearOnStart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disapearOnStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        gameObject.active = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
