4c4742a6847d9371f9222516bd60f37647772f24
Nguyen Phuong Huy <phuonghuynguyen@hotmail.com>
2018-11-05 12:36:43 -0500

    add animation to unityChan when clicking a number

---
+++ Smashing Baller/Assets/Script/UnityChanAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanAnimation : MonoBehaviour {
+
+    public Animator anim;
+
+    // Use this for initialization
+    void Start() {
+
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.GetKeyDown("1")) {
+
+            anim.Play("WAIT01", -1,0f);
+        }
+
+        if (Input.GetKeyDown("2")) {
+
+            anim.Play("WAIT02", -1, 0f);
+        }
+
+        if (Input.GetKeyDown("3")) {
+
+            anim.Play("WAIT03", -1, 0f);
+        }
+
+        if (Input.GetKeyDown("4")) {
+
+            anim.Play("WAIT04", -1, 0f);
+        }
+
+   
+
+    }
+}
